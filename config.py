e694054818c58595bf34e1333e5194f2a5821097
mikhailag570 <129588872+mikhailag570@users.noreply.github.com>
2023-08-20 14:38:16 +0400

    Add files via upload

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-open_weather_token = '1c716f8a1824e8b53f4d41ffccc8804d'
-tg_bot_token = '6166673957:AAG0gzrPy5jPmOB5Tt8Zdg1pSYnqtM0Yrec'
+open_weather_token = 'openweathermap_token'
+tg_bot_token = 'telegram_token'
